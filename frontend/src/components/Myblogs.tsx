f689752b7354b3facefa0c4fd46f85c647d01d4f
rudrasgithub <rudramanaidu99@gmail.com>
2024-04-01 20:22:51 +0530

    added myblogs

--- frontend/src/components/Myblogs.tsx
+++ frontend/src/components/Myblogs.tsx
@@ -2,7 +2,10 @@ import { Link } from "react-router-dom"
 import { Appbar } from "./Appbar"
 import { Dropdown } from "flowbite-react";
 import { Userblogs } from "../pages/Userblogs";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 export const Myblogs=()=>{
+    // const [rerender,setRerender]=useState(false);
     return <div>
         <Appbar/>
         <div className="mx-36 mt-16 max-w-screen-sm">
@@ -24,6 +27,16 @@ export const Myblogs=()=>{
                 </div>
                 <div className="flex justify-between mt-12">
                     <div className="text-md font-normal pl-5">
+                        {/* useEffect(()=>{
+                            async function Render(){
+                                await axios.get(`${BACKEND_URL}/api/v1/myblogs`,{
+                                    headers:{
+                                        Authorization:localStorage.getItem("token")
+                                    }
+                                })
+                            }
+                            Render()
+                        },[rerender]) */}
                         <Userblogs/>
                     </div>
                     <div className="pr-5">
@@ -36,13 +49,25 @@ export const Myblogs=()=>{
 }
 function ManageBlog(){
     return <div>
-        <Dropdown dismissOnClick={false} renderTrigger={() => <div>
+        <Dropdown dismissOnClick={false} renderTrigger={() =>
+             <div>
                 <svg className="hover:cursor-pointer w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
                     <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
                 </svg>
             </div>}>
             <Dropdown.Item>Edit blog</Dropdown.Item>
-            <Dropdown.Item>Delete blog</Dropdown.Item>
+            <Dropdown.Item className="text-red-600" onClick={DeleteBlog}>Delete blog</Dropdown.Item>
         </Dropdown>
     </div>
+}
+function DeleteBlog(){
+    async function deleteNow(){
+        await axios.delete(`${BACKEND_URL}/api/v1/blog/3`,{
+            headers:{
+                Authorization:localStorage.getItem("token")
+            }
+        })
+
+    }
+    deleteNow()
 }
